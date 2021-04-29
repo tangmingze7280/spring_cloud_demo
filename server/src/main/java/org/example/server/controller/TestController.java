f3b4e1b0ad3446506a83b6a9b685f14184f58a38
tangmingze <tangmingze@familydoctor.com>
2021-04-29 14:04:46 +0800

    111231

--- server/src/main/java/org/example/server/controller/TestController.java
+++ server/src/main/java/org/example/server/controller/TestController.java
@@ -1,14 +1,19 @@
 package org.example.server.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 @RequestMapping("test")
 public class TestController {
+    @Autowired
+    ApplicationContext applicationContext;
     @GetMapping("test1")
     public String test1(@RequestParam("msg")String msg){
         return msg+"test1";
@@ -20,5 +25,12 @@ public class TestController {
             throw new RuntimeException(msg);
         }
         return msg+"test2";
+    }
+    @GetMapping("test3")
+    public String test3(@RequestParam("msg")String msg){
+        if(msg.length()<2){
+            throw new RuntimeException(msg);
+        }
+        return msg+"port"+applicationContext.getEnvironment().getProperty("server.port");
     }
 }
